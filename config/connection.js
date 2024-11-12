515d707cea8621542527d99965c5efb063f192aa
Wesley Antico <Wesley@Wesleys-MacBook-Air.local>
2024-11-11 19:05:52 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL)
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
         host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
+        dialect: 'postgres'
       }
     );
-
-module.exports = sequelize;
