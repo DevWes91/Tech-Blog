34f2afa8329f9def62d522fcca9dfab255a5ab78
Wesley Antico <wesleyrantico@gmail.com>
2024-11-19 23:51:58 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres'
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+module.exports = sequelize;
